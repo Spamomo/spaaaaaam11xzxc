40da38d2ca51ec879fdfd10148dbca46024fff50
Spamomo <45368335+Spamomo@users.noreply.github.com>
2018-12-01 01:32:22 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,6 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-var prefix = "6";
+var prefix = "11";
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);  
@@ -8,7 +8,7 @@ client.on('ready', () => {
 
 client.on('ready', async() => {
 var server = "516296459805786114"; // ايدي السررفر
-var channel = "518083886153072640";//ايدي الروم
+var channel = "518190991753216004";//ايدي الروم
     setInterval(()=>{
     client.guilds.get(server).channels.get(channel).send('**Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , **')
     },305);
